b2fe8147078c29b6175c85790a3f7bcdc5c16f94
udi-collectors <DeviceCollectorsTeam@experian.com>
2026-02-24 22:06:14 -0600

    Adding Package.swift file

---
+++ Package.swift
@@ -0,0 +1,31 @@
+// swift-tools-version: 6.2
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "UDICollectorSDK",
+    platforms: [
+        .iOS(.v17)
+    ],
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "UDICollectorSDK",
+            targets: ["UDICollectorBinaryRemote"]
+        )
+    ],
+    targets: [
+        .binaryTarget(
+            name: "UDICollectorBinaryRemote",
+            url: "https://artifactory-mars.cd.genesaas.io/artifactory/device-collector-releases/com/udicollector/ios-collector/1.2.1/ios-collector-1.2.1-ios-lib.zip", // Replace with actual URL during build
+            checksum: "e7aeca45b7faf21cf205400d8d57207e4f0091d77ad05352cf259b0a2c0f8e9b" // Replace with actual checksum during build
+        )
+        // ,
+        // .binaryTarget(
+        //     name: "UDICollectorBinaryLocal",
+        //     path: "../sc-collector-sdk/target/frameworks/XCFramework/UDICollector.xcframework"
+        // )
+    ],
+    swiftLanguageModes: [.v5]
+)
